7de2baefd4d3f515fc5bf3836ada37100c6f9336
TheMhv <mhv.geral@gmail.com>
2024-11-18 08:22:50 -0300

    Feat: Shows a message when profile doest have any product

--- src/components/home/ShopInfo.tsx
+++ src/components/home/ShopInfo.tsx
@@ -72,57 +72,65 @@ export const ShopInfo = ({
       <header className="mt-24 px-4 md:px-8">
         <ShopHeader name={name} description={description} icon={icon} />
 
-        <div className="mx-auto mt-12 px-2 md:px-0 space-y-6">
-          <form role="search" className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center pl-3 pointer-events-none">
-              <Search className="w-4 h-4 text-white/60" aria-hidden="true" />
-            </div>
+        {products.length == 0 ? (
+          <div className="text-xl text-center mx-auto mt-12 px-2 md:px-0 space-y-6">
+            This profile dont get any product
+          </div>
+        ) : (
+          <div className="mx-auto mt-12 px-2 md:px-0 space-y-6">
+            <form role="search" className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center pl-3 pointer-events-none">
+                <Search className="w-4 h-4 text-white/60" aria-hidden="true" />
+              </div>
 
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearch}
-              className="bg-white/15 text-white text-sm rounded-lg outline-none border-2 border-transparent focus:border-white/15 block w-full pl-10 px-3 py-1.5"
-              placeholder="Search..."
-            />
-          </form>
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearch}
+                className="bg-white/15 text-white text-sm rounded-lg outline-none border-2 border-transparent focus:border-white/15 block w-full pl-10 px-3 py-1.5"
+                placeholder="Search..."
+              />
+            </form>
 
-          <nav className="flex items-center flex-wrap gap-2">
-            <button
-              onClick={handleOnClickTag}
-              value={""}
-              className={`rounded-full border-2 py-2 px-4 cursor-pointer transition hover:bg-white/15 hover:border-white/30 text-white text-sm font-medium whitespace-nowrap ${
-                activeTag == ""
-                  ? "bg-white/30 border-transparent"
-                  : "border-white/25 bg-transparent"
-              }`}
-            >
-              All
-            </button>
-
-            {tags.map((tag, index) => (
+            <nav className="flex items-center flex-wrap gap-2">
               <button
-                key={index}
                 onClick={handleOnClickTag}
-                value={tag}
+                value={""}
                 className={`rounded-full border-2 py-2 px-4 cursor-pointer transition hover:bg-white/15 hover:border-white/30 text-white text-sm font-medium whitespace-nowrap ${
-                  activeTag == tag
+                  activeTag == ""
                     ? "bg-white/30 border-transparent"
                     : "border-white/25 bg-transparent"
                 }`}
               >
-                {tag}
+                All
               </button>
-            ))}
-          </nav>
-        </div>
+
+              {tags.map((tag, index) => (
+                <button
+                  key={index}
+                  onClick={handleOnClickTag}
+                  value={tag}
+                  className={`rounded-full border-2 py-2 px-4 cursor-pointer transition hover:bg-white/15 hover:border-white/30 text-white text-sm font-medium whitespace-nowrap ${
+                    activeTag == tag
+                      ? "bg-white/30 border-transparent"
+                      : "border-white/25 bg-transparent"
+                  }`}
+                >
+                  {tag}
+                </button>
+              ))}
+            </nav>
+          </div>
+        )}
       </header>
 
-      <section className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-y-6 pb-20">
-        {filteredProducts.map((product, index) => (
-          <ProductCard key={index} product={product.asJson()} />
-        ))}
-      </section>
+      {products.length >= 1 && (
+        <section className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-y-6 pb-20">
+          {filteredProducts.map((product, index) => (
+            <ProductCard key={index} product={product.asJson()} />
+          ))}
+        </section>
+      )}
 
       <Footer />
     </>
