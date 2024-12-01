de8d07641e2d946bc52d9419cdc9baf1dcd38a05
TheMhv <mhv.geral@gmail.com>
2024-12-01 12:40:50 -0300

    Feat: Create order and get invoice functions

--- src/components/checkout/CheckoutForm.tsx
+++ src/components/checkout/CheckoutForm.tsx
@@ -6,6 +6,15 @@ import { ShippingInformationForm } from "./ShippingInformation";
 import { InvoiceForm } from "./InvoiceForm";
 import { SummaryForm } from "./SummaryForm";
 import { NostrContext } from "../NostrProvider";
+import { useCart } from "../cart/CartContext";
+import {
+  createOrderContent,
+  OrderContent,
+  OrderItem,
+  PaymentRequestContent,
+} from "@/lib/nostr/checkout";
+import { PublicKey } from "@rust-nostr/nostr-sdk";
+import QRCode from "qrcode";
 
 export type checkoutPageFormData = {
   nostr: string;
@@ -41,16 +50,6 @@ export const CheckoutForm = ({ shopPubKey }: { shopPubKey: string }) => {
     }));
   }, [client, pubKey]);
 
-  const [step, setStep] = useState<number>(1);
-
-  const [formData, setFormData] = useState({
-    email: "",
-    telephone: "",
-    name: "",
-    address: "",
-    nostr: "",
-  } as checkoutPageFormData);
-
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
     const { name, value } = e.target as HTMLInputElement;
 
@@ -63,22 +62,94 @@ export const CheckoutForm = ({ shopPubKey }: { shopPubKey: string }) => {
   const handleNextStep = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (step === 1 && formData.email) {
-      setStep(2);
+    if (step === 1 && formData.nostr.length >= 1) {
+      return setStep(2);
+    }
+
+    if (
+      step === 2 &&
+      formData.name.length >= 1 &&
+      formData.address.length >= 1
+    ) {
+      return setStep(3);
+    }
+
+    if (step === 3) {
+      handleSendOrder().then((orderId) =>
+        handleRequestPayment(orderId).then(() => setStep(4))
+      );
+
       return;
     }
 
-    if (step === 2 && formData.name && formData.address) {
-      setStep(3);
-      return;
-    }
-
-    setStep(step + 1);
+    return setStep(step + 1);
   };
 
   const handlePrevStep = () => {
     setStep(step - 1);
   };
+
+  const handleSendOrder = async () => {
+    const shippingId: string = cartContext.items
+      .filter((item) => item.shipping)
+      .map((item) => item.shipping?.method)
+      .join(",");
+
+    const items: OrderItem[] = cartContext.items.map((item) => {
+      return {
+        product_id: item.id,
+        quantity: item.quantity,
+      };
+    });
+
+    const orderContent: OrderContent = createOrderContent(
+      formData.nostr,
+      shippingId,
+      items,
+      formData.name,
+      formData.address,
+      formData.message,
+      formData.telephone,
+      formData.email
+    );
+
+    await client?.sendPrivateMsg(
+      PublicKey.parse(shopPubKey),
+      JSON.stringify(orderContent)
+    );
+
+    return orderContent.id;
+  };
+
+  const handleRequestPayment = async (orderId: string) => {
+    const response = await fetch("/api/requestPayment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        orderId: orderId,
+        npub: pubKey?.toBech32(),
+        amount: Math.ceil(cartContext.totalPrice + cartContext.totalShipping),
+      }),
+    });
+
+    const data: PaymentRequestContent = await response.json();
+
+    const lightningInvoice = data.payment_options.find(
+      (option) => option.type == "ln"
+    )?.link;
+
+    if (!lightningInvoice) {
+      throw new Error("Unable to create lightning invoice");
+    }
+
+    setInvoice(lightningInvoice);
+    QRCode.toDataURL(lightningInvoice).then((qrCode) => setInvoiceQR(qrCode));
+  };
+
+  const [invoice, setInvoice] = useState<string | undefined>();
+  const [invoiceQR, setInvoiceQR] = useState<string | undefined>();
 
   return !client || !pubKey ? (
     <>
@@ -145,8 +216,12 @@ export const CheckoutForm = ({ shopPubKey }: { shopPubKey: string }) => {
 
       <div className="bg-gray-800 border border-gray-700 rounded-lg p-6">
         <form onSubmit={handleNextStep}>
-          {step === 4 ? (
-            <InvoiceForm handlePrevStep={handlePrevStep} />
+          {step === 4 && invoice && invoiceQR ? (
+            <InvoiceForm
+              invoice={invoice}
+              qrCode={invoiceQR}
+              handlePrevStep={handlePrevStep}
+            />
           ) : step === 3 ? (
             <SummaryForm cart={cartContext} handlePrevStep={handlePrevStep} />
           ) : step === 2 ? (
